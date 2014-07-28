9d12f19ef7b14c53758a753edd397635a45f8794
yababay <yababay@pochta.ru>
2014-07-28 20:35:26 +0400

    boilerplate

---
+++ js/main.js
@@ -0,0 +1,18 @@
+requirejs.config({
+  baseUrl: 'js',
+  paths: {
+	jquery:        'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
+    jquery_ui:     'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
+    jquery_mobile: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.1/jquery.mobile.min',
+    mapbox:        'https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox',
+    maplabel:      'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-label/v0.2.1/leaflet.label',
+  }
+
+  shim: {
+    jquery_mobile: {deps: ['jquery']},
+    jquery_ui:     {deps: ['jquery_mobile']},
+  }
+})
+
+require(['index'])
+
