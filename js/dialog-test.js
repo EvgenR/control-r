93fb5d9a4e26882831aba97ef66a145d2e8e5ec0
yababay <yababay@pochta.ru>
2014-08-01 01:59:36 +0400

    dialog3

--- js/dialog-test.js
+++ js/dialog-test.js
@@ -2,5 +2,6 @@ define(['jquery_ui'], function(){
   $(function(){	
     //$('.control-r-dialog-title').css('margin-bottom', '25px')
 	$('#dialog-test').dialog()
+	$('.control-r-dialog').css('background-image', 'url("img/dialog-background.jpg")')  
   })
 })
