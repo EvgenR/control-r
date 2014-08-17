9a0a62cd0b7346ea4c5fcb5075df31b385a47a7e
yababay <yababay@pochta.ru>
2014-08-17 18:28:20 +0400

    eugen-dlg4

--- dialog/js/dialog-test.js
+++ dialog/js/dialog-test.js
@@ -2,8 +2,7 @@ define(['jquery_ui'], function(){
 	$(function(){	
 		$('button').button().click(function(){
 			var dlgType = $(this).attr('id')
-		    var dlg = $('#control-r-dialog').clone()
-			$(dlg).dialog('open')
+		    $('#control-r-dialog').dialog()
 		})    
 	})
 })
