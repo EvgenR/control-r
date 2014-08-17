2289528ea37ef60e66d596e743001d9622068248
yababay <yababay@pochta.ru>
2014-08-17 21:59:16 +0400

    eugen-dlg4

--- dialog/js/dialog-test.js
+++ dialog/js/dialog-test.js
@@ -2,6 +2,7 @@ define(['jquery_ui'], function(){
 	$(function(){	
 		$('a').click(function(){
 			var dlgType = $(this).attr('href').substring(1)
+			alert(dlgType)
 		    var dlg = $('.control-r-dialog').clone()
 			var tabs = $(dlg).find('.control-r-dialog-tabs').tabs()
 			//$(dlg).dialog()
