cb25a5341125cc22993faaef9763bb542e430ef8
yababay <yababay@pochta.ru>
2014-08-18 11:08:10 +0400

    eugen-dlg4

--- dialog/js/dialog.js
+++ dialog/js/dialog.js
@@ -1,18 +1,23 @@
-requirejs.config({
-  urlArgs: "bust=" + (new Date()).getTime(),		
-  baseUrl: 'js',
-  paths: {
-	jquery:        'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
-    jquery_ui:     'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
-    jquery_mobile: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.1/jquery.mobile.min',
-  },
-
-  shim: {
-    jquery_mobile: {deps: ['jquery']},
-//    jquery_ui:     {deps: ['jquery_mobile']},
-    jquery_ui:     {deps: ['jquery']},
-  }
+define(['dialog-id-listeners', 'jquery_ui'], function(setInputListeners){
+	$(function(){	
+		$('a').click(function(){
+			var dialogType = $(this).attr('href').substring(1)
+		    var dlg = $('.control-r-dialog').clone().attr('class', '')
+			var randomHrefs = [Math.random(), Math.random()]
+			var tabAnchors  = $(dlg).find('ul a') 
+			var tabContents = $(dlg).find('.control-r-dialog-tab') 
+			for(var i = 0; i < 2; i++){
+              $(tabAnchors[i]).attr('href', '#' + randomHrefs[i])
+              $(tabContents[i]).attr('id',  '' + randomHrefs[i])
+			}
+			$(dlg).find('.control-r-dialog-tabs').tabs()
+			$(dlg).dialog({width: 640, height: 480})
+		    window.dialogCommonTab  = tabContents[0]
+		    window.dialogSpecialTab = tabContents[1]
+			$(tabContents[1]).load(dialogType + '.html', function(dat){
+			  require([dialogType])
+			})
+		    setInputListeners($(tabContents[0]).find('input'))
+		})    
+	})
 })
-
-require(['dialog-test'])
-
