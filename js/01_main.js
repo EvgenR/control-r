0c6028eea9a438ec94ee992e0b0448e660fd7a21
mabel <pochta@yababay.ru>
2014-09-03 09:36:01 -0400

    mapbox

--- js/01_main.js
+++ js/01_main.js
@@ -1,11 +1,12 @@
 requirejs.config({
-  //urlArgs: "bust=" + (new Date()).getTime(),		
+  waitSeconds: 30,		
+  urlArgs: "bust=" + (new Date()).getTime(),		
   baseUrl: 'js',
   paths: {
 	jquery:        'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min',
     jquery_ui:     'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min',
     jquery_mobile: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-mobile/1.4.1/jquery.mobile.min',
-    mapbox:        'https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox',
+    mapbox:        'https://api.tiles.mapbox.com/mapbox.js/v2.0.1/mapbox',
     maplabel:      'https://api.tiles.mapbox.com/mapbox.js/plugins/leaflet-label/v0.2.1/leaflet.label',
 	geomap:        '50_geomap'
   },
--- js/50_geomap.js
+++ js/50_geomap.js
@@ -1,19 +1,13 @@
 define(['21_geoobjects'], function(Geoobjects){
-
-  var mapboxTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/alexru2014.ia5picp3/{z}/{x}/{y}.png', {
-	attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
-  });
-
   var mapDiv = $('#geo-map').height($(window).height())[0]		
-  var map = L.map(mapDiv, {zoomControl: false}).addLayer(mapboxTiles).setView([Geoobjects.view.lat, Geoobjects.view.lng], Geoobjects.view.zoom);
-
-
+  L.mapbox.accessToken = 'pk.eyJ1IjoiYWxleHJ1MjAxNCIsImEiOiJJMHQ4NnFRIn0.3BBwGZ_YzX1lGX_yzSjcvg'
+  var map = L.mapbox.map(mapDiv, 'alexru2014.ia5picp3').setView([Geoobjects.view.lat, Geoobjects.view.lng], Geoobjects.view.zoom)
   for(var i in Geoobjects){
       if(i == 'view') continue
   	  var val  = Geoobjects[i]
 	  var hash = '#page-' + i
-	  //var marker = L.marker([val.lat, val.lng]).bindLabel(val.label, {noHide: true}).addTo(map) //.on('click', function(){
-	  //setClick(marker, hash)
+	  var marker = L.marker([val.lat, val.lng]).bindLabel(val.label, {noHide: true}).addTo(map)
+	  setClick(marker, hash)
   }
   $.mobile.loading('hide')
 })
